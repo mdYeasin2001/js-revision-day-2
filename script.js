cdfac391481d6f286c21f77732d5cd37cf1dc9dd
Yeasin <yeasin30112001@gmail.com>
2021-02-19 23:55:27 -0600

    practice type conversion, integer, float, parseInt, parseFloat, typeof and others

--- script.js
+++ script.js
@@ -20,7 +20,15 @@ let friendsHave = 10;
 let promise = 'I will hard work to become a Web Developer';
 let up =  promise.toUpperCase();
 let low = promise.toLowerCase();
-console.log(low, up, promise);
-console.log(promise.indexOf("won't"));
+// console.log(low, up, promise);
+// console.log(promise.indexOf("won't"));
 let promiseInArr = promise.split(' ');
-console.log(promiseInArr[7]);
+// console.log(promiseInArr[7]);
+
+
+/*-------------- integer, float, parseInt, parseFloat, type conversion, typeof, string to number by using '+' sign before the string, number to string using "" empty string -----------------*/
+let integerNum = 5;
+let floatNum = 3.05;
+let stringNumber = +'10' // +
+console.log(integerNum + "");
+console.log(stringNumber);
