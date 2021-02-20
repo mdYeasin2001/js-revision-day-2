3340702542a186b0703066e8eb083013f3edcd42
Yeasin <yeasin30112001@gmail.com>
2021-02-19 19:05:32 -0600

    practice variable and variable type

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-// variable
+/*---------------variable------------*/
 let money = 100;
 money = 80;
-console.log(money);
+// console.log(money);
 
 const friendName = 'ali';
-friendName = 'abdul';
-console.log(friendName);
+// friendName = 'abdul'; 
+// console.log(friendName);
+
+
+/*------------------ variable type---------------- */
+//string, number, boolean
+let friend = 'abdul';
+let haveMoney = true;
+const haveGirlFriends = false;
+let friendsHave = 10;
