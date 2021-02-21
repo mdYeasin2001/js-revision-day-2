ddc7d4cb7faaa0823faa2d13b7e87c1c1a0a3b3a
Yeasin <yeasin30112001@gmail.com>
2021-02-20 21:54:30 -0600

    tarnary

---
+++ turnary.js
@@ -0,0 +1,10 @@
+let money = 100;
+let food;
+if(money > 100){
+    food = "biriany";
+}else{
+    food = "alur vorta";
+}
+let food2 = money > 100 ? 'barger' : 'dal-vat';
+console.log(food);
+console.log(food2);
