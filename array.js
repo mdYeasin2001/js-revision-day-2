9cc32a96037592818ab78b9d61c88acdc6d534bc
Yeasin <yeasin30112001@gmail.com>
2021-02-20 18:44:57 -0600

    array

--- array.js
+++ array.js
@@ -1,4 +1,5 @@
 const friends = ['ali', 'simul', 'asraful', 'nasreen'];
+const numbers = [3, 5, 8];
 friends[1] = 'moniur';
 console.log(friends.indexOf('nasreen'));
 console.log(friends);
@@ -12,3 +13,4 @@ console.log(friends.length);
 friends.shift();
 friends.unshift('shefa')
 console.log(friends);
+console.log(friends.concat(...numbers));
